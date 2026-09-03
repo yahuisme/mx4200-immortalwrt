cf65e7b53a799ab36101430f08f3113a62bd28f9
yahuisme <40222380+yahuisme@users.noreply.github.com>
2026-09-03 14:19:40 +0800

    chore: drop manual package input

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -89,11 +89,6 @@ if [ -f "$GITHUB_WORKSPACE/Config/PRIVATE.txt" ]; then
 	cat $GITHUB_WORKSPACE/Config/PRIVATE.txt >> ./.config
 fi
 
-#手动调整的插件
-if [ -n "$WRT_PACKAGE" ]; then
-	echo -e "$WRT_PACKAGE" >> ./.config
-fi
-
 #无WIFI配置标志
 if [[ "${WRT_CONFIG,,}" == *"wifi"* && "${WRT_CONFIG,,}" == *"no"* ]]; then
 	echo "WRT_WIFI=wifi-no" >> $GITHUB_ENV
