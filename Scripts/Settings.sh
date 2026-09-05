1286f1ba930f5807b17060700aad0a75f536615f
yahuisme <40222380+yahuisme@users.noreply.github.com>
2026-09-05 12:00:37 +0800

    fix: normalize default distribution name in version.mk

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -58,6 +58,11 @@ sed -i "s/%D %V %C/%D %C/g" ./package/base-files/files/etc/openwrt_release
 sed -i "s/%D %V %C/%D %C/g" ./package/base-files/files/usr/lib/os-release
 sed -i "s/%D %V, %C/%D %C/g" ./package/base-files/files/etc/banner
 
+if [ -f "./include/version.mk" ]; then
+	sed -i 's/VERSION_DIST,ImmortalWRT/VERSION_DIST,ImmortalWrt/g' ./include/version.mk
+	sed -i 's/VERSION_MANUFACTURER,ImmortalWRT/VERSION_MANUFACTURER,ImmortalWrt/g' ./include/version.mk
+fi
+
 mkdir -p ./package/base-files/files/etc/uci-defaults/
 cp "$GITHUB_WORKSPACE/files/etc/uci-defaults/99-mx4200-defaults" ./package/base-files/files/etc/uci-defaults/99-mx4200-defaults
 chmod +x ./package/base-files/files/etc/uci-defaults/99-mx4200-defaults
